0a6bba752495586604594fbe86f6dd43d73f1a5a
Yoko <yokoujihara622@gmail.com>
2021-10-30 16:12:01 +1000

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+
+const Form = (props) => {
+  
+  return (
+    <form>
+      <input type="text" name="city" placeholeder="place name" onChange={e => props.setCity(e.target.value)}/>
+      <button type="submit" onClick={props.getWeather}>Get Weather</button>
+    </form>
+  )
+}
+
+export default Form
